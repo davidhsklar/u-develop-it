74c4bb7a2b78239fd33302b1cece5f5f0ff11cd0
David Sklar <davidhsklar@gmail.com>
2022-05-04 18:48:59 -0400

    set up SQL connection but no passwords posted to github

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+const mysql = require('mysql2');
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+        password: '!!',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+
+// GET route
+
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+  });
+
+  // database query
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+// Default (catchall) response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
